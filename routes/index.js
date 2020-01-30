14ca1678e6cc7e447968a54d454f2e041ca32581
afd <afd@alup5.academiapostal.es.local>
2020-01-30 17:42:37 +0100

    comprobación de existencia del producto

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,31 @@
 var express = require('express');
 //indica que requiere express
 var router = express.Router();
+var products= require("../models/products");
 // router es el que me indica todas las rutas,direcciones,asocia rutas con funciones
 /* GET home page. */
 //cada vez que le pida router por get me aplica esa función, separo todas las rutas en funciones
 //req=request petición , res=response respuesta, next=siguiente
 router.get('/', function(req, res, next) {
-  const products = [
-    {nombre:"Microondas", precio:45, existencias:6, imagen:"microondas.jpg"},
-    {nombre:"frigorífico", precio:2500, existencias:4, imagen:"frigo.jpg"},
-    {nombre:"lámpara", precio:20, existencias:14, imagen:"lampara.jpg"},
-    {nombre:"tv",precio:500, existencias:0, imagen:"tv.jpg"},
-    {nombre:"lavadora",precio:290,existencias:3, imagen: "lavadora.jpg"},
-    {nombre:"secadora",precio:180, existencias:5, imagen: "secadora.jpg"}
-  ];
+  
   //respondeme con el render(dibujo en html)
-  res.render('index', { title: 'Silverado', products });
+  res.render('index', { title: 'Silverado', products });  
+});
+//hacemos una ruta que es variable y requerimos de la lista de los productos por su referencia
+//busco el producto que coincide con la referencia ref
+router.get('/products/:ref', function(req, res, next) {
+  var ref = req.params.ref;
+  const product = products.find(function(p){
+    return p.ref==ref;
+  });
+//con el render pasamos  los productos {product} a la plantilla 'product'
+if (product){
+ 
+  res.render ('product', {product});
+}else{
+  //si no existe ese preducto me redirecciona a la página de error
+  res.redirect("/error");
+}
 });
 
 module.exports = router;
