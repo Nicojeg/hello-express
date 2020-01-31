098bd92a1b4391c43dfde65b05ad0591fbaffc6c
afd <afd@alup5.academiapostal.es.local>
2020-01-31 19:08:47 +0100

    Poner en español

---
+++ models/users.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {usuario:"jairo", password:"123456"},
+    {usuario:"pepito", password:"654321"},
+    {usuario:"Nico", password:"121212"}
+];
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 //indica que requiere express
 var router = express.Router();
-var products= require("../models/products");
+var products = require("../models/products.js");
+var users = require('../models/users.js');
 // router es el que me indica todas las rutas,direcciones,asocia rutas con funciones
 /* GET home page. */
 //cada vez que le pida router por get me aplica esa función, separo todas las rutas en funciones
@@ -15,17 +16,61 @@ router.get('/', function(req, res, next) {
 //busco el producto que coincide con la referencia ref
 router.get('/products/:ref', function(req, res, next) {
   var ref = req.params.ref;
+  
   const product = products.find(function(p){
     return p.ref==ref;
   });
-//con el render pasamos  los productos {product} a la plantilla 'product'
-if (product){
- 
-  res.render ('product', {product});
-}else{
-  //si no existe ese preducto me redirecciona a la página de error
-  res.redirect("/error");
-}
+
+  
+  if (product){
+  //con el render pasamos  los productos {product} a la plantilla 'product'
+    res.render ('product', {product});
+  }else{
+    //si no existe ese preducto me redirecciona a la página de error
+    res.redirect("/error");
+  }
 });
+
+var cesta =[]; //provisional
+
+router.post("/comprar", function(req, res, next){
+    const ref =req.body.ref;
+    const product = products.find(function(p){
+      return p.ref==ref;
+    });
+
+ //añadimos producto a la cesta
+  cesta.push(product);
+ //redirigimos a página de productos
+ res.redirect("/");
+ });
+ router.get("/login", function (req, res, next){
+ //cuando se pregunte por login tiene que hacer la función
+   res.render("login");
+  });
+  /**procesamiento del formulario de login. Obtiene los datos del formulario en la petición (req) 
+   * y comprueba si hay algún usuario con ese nombre 
+   * y contraseña
+   * si coincide genera una cookie y dirige a la página principal.
+   * si no coincide, vuelve a cargar la página de login para mostrar un error
+   */
+
+  router.post("/login", function (req, res, next){
+  /**const usuario = req.body.usuario;
+   const password = req.body.password;*/
+  /** sería lo mismo que hacer de esta otra manera */
+    const {usuario, password} = req.body;
+    const user = users.find(function (u) {
+      return (u.usuario == usuario && u.password == password);
+    
+    });
+    if (user){
+      res.redirect("/");
+    }else{
+      //meter un mensaje de error en pantalla
+      res.render("login");
+    }
+  });
+
 
 module.exports = router;
