e3672b905eeb236570afad9c6ab964580c8e938b
afd <afd@alup5.academiapostal.es.local>
2020-02-03 20:41:22 +0100

    products listo

--- models/products.js
+++ models/products.js
@@ -1,11 +1,11 @@
+module.exports= 
+[
+ {ref:"1001",nombre:"Microondas", precio:45, existencias:6, imagen:"microondas.jpg"},
+ {ref:"1002",nombre:"frigorífico", precio:2500, existencias:4, imagen:"frigo.jpg"},
+ {ref:"1003",nombre:"lámpara", precio:20, existencias:14, imagen:"lampara.jpg"},
+ {ref:"1004",nombre:"tv",precio:500, existencias:0, imagen:"tv.jpg"},
+ {ref:"1005",nombre:"lavadora",precio:290,existencias:3, imagen: "lavadora.jpg"},
+ {ref:"1006",nombre:"secadora",precio:180, existencias:5, imagen: "secadora.jpg"}
+];
 
-    module.exports= 
-   [
-    {ref:"1001",nombre:"Microondas", precio:45, existencias:6, imagen:"microondas.jpg"},
-    {ref:"1002",nombre:"frigorífico", precio:2500, existencias:4, imagen:"frigo.jpg"},
-    {ref:"1003",nombre:"lámpara", precio:20, existencias:14, imagen:"lampara.jpg"},
-    {ref:"1004",nombre:"tv",precio:500, existencias:0, imagen:"tv.jpg"},
-    {ref:"1005",nombre:"lavadora",precio:290,existencias:3, imagen: "lavadora.jpg"},
-    {ref:"1006",nombre:"secadora",precio:180, existencias:5, imagen: "secadora.jpg"}
-  ];
   
