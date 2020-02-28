e402f2aff9e2a0dc3180afad9c79e291f743ea1a
afd <afd@alup5.academiapostal.es.local>
2020-02-28 20:27:51 +0100

    creacion de llaves foráneas entre tablas y creación de una nueva

--- models/index.js
+++ models/index.js
@@ -4,9 +4,17 @@ const Producto = require('./products')
 const Usuario = require('./users')
 const Carrito = require('./carrito')
 const Pedido = require('./pedido')
+const ProductoCarrito = require('./producto-carrito')
+
  
 Usuario.hasOne(Carrito);
 Carrito.belongsTo(Usuario);
+
+Usuario.hasMany(Pedido);
+Pedido.belongsTo(Usuario);
+
+Carrito.belongsToMany(Producto,{through:ProductoCarrito});
+Producto.belongsToMany(Carrito,{through:ProductoCarrito});
 
 
 
--- models/pedido.js
+++ models/pedido.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
  const sequelize = require('./db');
 
 const Pedido = sequelize.define('pedido', {
-    estado:Sequelize.ENUM('PDTE-PAGO','PAGADO','CANCELADO','EN_TRANSITO','COMPLETO')
+    estado:Sequelize.ENUM('PDTE-PAGO','PAGADO','CANCELADO','EN_TRANSITO','COMPLETO'),
+    direccionEntrega:Sequelize.STRING(200)
 });
 module.exports=Pedido;
