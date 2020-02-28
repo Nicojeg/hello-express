2f0d22a7151f51444f0cf3a4951bd128d540f38d
afd <afd@alup5.academiapostal.es.local>
2020-02-28 17:28:38 +0100

    creando modelo de usuarios y tabla en xampp

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 
 const sequelize =require('./db')//importaciones
 const Producto = require('./products')
+const Usuario = require('./users')
 
 //Finalmente conectamos con la base de datos
 sequelize
@@ -13,8 +14,9 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
   module.exports ={//exportaciones
-      Producto
+      Producto,
+      Usuario
       //Pedido
-      //Usuario
+      
       //Carrito
   }
--- models/users.js
+++ models/users.js
@@ -1,5 +1,17 @@
-module.exports = [
+const Sequelize = require('sequelize');
+ const sequelize = require('./db');
+
+const Usuario = sequelize.define('users', {
+    
+    email: Sequelize.STRING,
+    password:Sequelize.STRING,
+    nombre: Sequelize.STRING,
+    apellidos: Sequelize.STRING
+    
+  });  
+module.exports=Usuario;
+/*module.exports = [
     {usuario:"jairo", password:"123456"},
     {usuario:"pepito", password:"654321"},
     {usuario:"Nico", password:"121212"}
-];
+];*/
